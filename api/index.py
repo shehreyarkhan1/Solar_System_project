cfac8e4cf4317e6c6017e314e3c0faa12273494f
shehreyarkhan123 <shehreyarkhan123>
2025-09-29 06:02:44 +0500

    done8

--- api/index.py
+++ api/index.py
@@ -7,6 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solar_system_drf.settings')
 # Get the WSGI application
 application = get_wsgi_application()
 
-# For Vercel deployment
-def handler(request):
-    return application(request)
+# Export the WSGI application as the handler for Vercel
+handler = application
