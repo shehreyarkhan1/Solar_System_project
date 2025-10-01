63cc75569efd33f996e723449bb8046a9e944139
shehreyarkhan123 <shehreyarkhan123>
2025-10-02 00:11:03 +0500

    done 21

--- solar_system_drf/settings.py
+++ solar_system_drf/settings.py
@@ -256,11 +256,11 @@ LOGGING = {
     },
 }
 # Cloudinary config
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
-}
+# CLOUDINARY_STORAGE = {
+#     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+#     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+#     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+# }
 
 # Set Cloudinary as default storage for media
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
